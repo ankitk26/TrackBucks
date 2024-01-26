c37a1750a1b8ad22a9f3970e37ed20902dc0cc52
Ankit Kumar <myselfankit51@gmail.com>
2024-01-27 01:49:23 +0530

    fixed default year in monthly transactions screen

--- lib/providers/state_providers.dart
+++ lib/providers/state_providers.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final drawerKeyProvider = StateProvider((ref) => GlobalKey<ScaffoldState>());
-final currentYearProvider = StateProvider((ref) => 2023);
+final currentYearProvider = StateProvider((ref) => DateTime.now().year);
 final currentMonthProvider = StateProvider((ref) => DateTime.now().month);
