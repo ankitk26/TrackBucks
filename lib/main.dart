2a6486236b11b633fee8ce561648ef233eda2f8a
Ankit Kumar <myselfankit51@gmail.com>
2024-02-04 17:42:10 +0530

    added main.dart

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:trackbucks/config/config.dart';
+import 'package:trackbucks/features/transactions/presentation/screens/export.dart';
+import 'package:trackbucks/utils/utils.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+  await Supabase.initialize(
+    url: dotenv.get('SUPABASE_URL'),
+    anonKey: dotenv.get('SUPABASE_KEY'),
+  );
+  runApp(const ProviderScope(child: RootApp()));
+}
+
+class RootApp extends StatelessWidget {
+  const RootApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: appTheme.copyWith(
+          textTheme: GoogleFonts.poppinsTextTheme(
+            ThemeData.dark().textTheme,
+          ),
+        ),
+        home: const HomeScreen(),
+        onGenerateRoute: generateRoutes,
+      ),
+    );
+  }
+}
